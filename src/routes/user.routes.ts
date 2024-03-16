9a6d3388586623f6ff99a9d7bc4ce6f52b0b428e
Si Thu Hpyo <sandaraion@gmail.com>
2024-03-16 10:43:05 +0700

    remove migration.ts

--- src/routes/user.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Elysia } from "elysia";
-import { dbPlugin } from "../plugin/db.plugin";
-
-const userRoutes = new Elysia().use(dbPlugin()).get("/user", ({ db }) => {
-  return db.query.userTable.findMany();
-});
-
-export default userRoutes;
